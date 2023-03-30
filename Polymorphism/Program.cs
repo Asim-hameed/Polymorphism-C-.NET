4b378f4df4f1abb44e40af2f1263c02366679cd3
Asim-hameed <asimhameed31@gmail.com>
2023-03-30 23:53:31 +0500

    Static Polymorphism/ Operator Overloading

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace Polymorphism
 {
+    class overloading
+    {
+        public string str;
+        public int num;
+
+        public static overloading operator +(overloading o1, overloading o2)
+        {
+            overloading overloading = new overloading();
+            overloading.str = o1.str+" " + o2.str;
+            overloading.num = o1.num+o2.num;
+            return overloading;
+        }
+    }
     class Program
     {
+        //Method Overloading starts here
         public void Add(int a, int b)
         {
             Console.WriteLine(a+b);
@@ -10,15 +24,48 @@
         {
             Console.WriteLine(a+b);
         }
-        public void Add()
+        public void Add(string a,string b)
         {
 
         }
+        //Method Overloading ends here
+
+
+        //Operator Overloading starts here
+
+
+
+        //operator Overloading ends here
+
+
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.Add(2,5);
-            program.Add(3,4.777);
+            //Method Overloading starts here
+            
+            //program.Add(2,5);
+            //program.Add(3,4.777);
+            
+            //Method Overloading ends here
+
+            
+            //Operator Overloading starts here
+
+            overloading overloading = new overloading();
+            overloading.str = "Asim";
+            overloading.num = 100;
+
+            overloading overlaoding1 = new overloading();
+            overlaoding1.str = "Hameed";
+            overlaoding1.num= 200;
+
+            overloading overloading2 = new overloading();
+            overloading2 = overloading + overlaoding1;
+            Console.WriteLine(overloading2.str);
+            Console.WriteLine(overloading2.num);
+
+            //operator Overloading ends here
+
         }
     }
 }
